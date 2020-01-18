ea9b7f5810f45e1fab3a1d8c467cc655d2cb9d14
Debdut Goswami <debdutgoswami@gmail.com>
2020-01-18 23:15:08 +0530

    UPDATE education

--- app.py
+++ app.py
@@ -11,13 +11,22 @@ def home():
         return render_template('home.html')
     else:
         data = dict(request.form)
-        exp = list()
-        for cname, cpos, cstart, cend, cdescription in zip(request.form.getlist('cname'),request.form.getlist('cpos'),request.form.getlist('cstart'),request.form.getlist('cend'),request.form.getlist('cdescription')):
+        exp, skills, edu = list(), list(), list()
+
+        # extracting experience
+        for cname, cpos, cstart, cend, cdescription, clocation in zip(request.form.getlist('cname'),request.form.getlist('cpos'),request.form.getlist('cstart'),request.form.getlist('cend'),request.form.getlist('cdescription'),request.form.getlist('clocation')):
             exp.append(dict({
-                'cname': cname, 'cpos': cpos, 'cstart': cstart, 'cend': cend, 'cdescription':cdescription
+                'cname': cname, 'cpos': cpos, 'cstart': cstart, 'cend': cend, 'cdescription':cdescription, 'clocation':clocation
             }))
-        data['skills'] = data['skills'].split(', ')
-        return render_template('cv.html',len_skills=len(data['skills']) , data=data, experience=exp, len_exp=len(exp))
+        # extracting skills
+        for skill in request.form.getlist('skill'):
+            skills.append(skill)
+        # extracting education
+        for clg, degree, gpa, start, end in zip(request.form.getlist('clg'), request.form.getlist('degree'), request.form.getlist('gpa'), request.form.getlist('start'), request.form.getlist('end')):
+            edu.append({
+                'clg':clg, 'degree': degree, 'gpa': gpa, 'start': start, 'end': end
+            })
+        return render_template('resume.html', skills=skills,len_skills=len(skills) , data=data, experience=exp, len_exp=len(exp), edu=edu, len_edu=len(edu))
 
 @app.route('/howtouse')
 def howtouse():

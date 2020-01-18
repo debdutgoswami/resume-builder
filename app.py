d3d844035118984c4e70fd1a9c0b94818f137f8f
Debdut Goswami <debdutgoswami@gmail.com>
2020-01-18 11:47:28 +0530

    ADDED workexperience support

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -9,8 +11,13 @@ def home():
         return render_template('home.html')
     else:
         data = dict(request.form)
+        exp = list()
+        for cname, cpos, cstart, cend, cdescription in zip(request.form.getlist('cname'),request.form.getlist('cpos'),request.form.getlist('cstart'),request.form.getlist('cend'),request.form.getlist('cdescription')):
+            exp.append(dict({
+                'cname': cname, 'cpos': cpos, 'cstart': cstart, 'cend': cend, 'cdescription':cdescription
+            }))
         data['skills'] = data['skills'].split(', ')
-        return render_template('cv.html',len_skills=len(data['skills']) , data=data)
+        return render_template('cv.html',len_skills=len(data['skills']) , data=data, experience=exp, len_exp=len(exp))
 
 @app.route('/howtouse')
 def howtouse():

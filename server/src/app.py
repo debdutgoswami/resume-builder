f39f7493ff437ddcbbda1b27b799e002a04dc37b
Debdut Goswami <debdutgoswami@gmail.com>
2020-02-09 16:48:17 +0530

    ADDED bootstrap navbar

--- server/src/app.py
+++ server/src/app.py
@@ -62,4 +62,4 @@ def about():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT',8080)), debug=True)
